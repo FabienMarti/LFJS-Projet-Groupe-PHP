894f7d1757cedbe8fcbb58d0ea2e71d8c190430e
FabienMarti <martiretti.fabien@gmail.com>
2020-07-01 10:50:10 +0200

    g

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php include 'views/parts/header.php' ?>
+<?php 
+    $title = 'Accueil';
+    include 'views/parts/header.php';
+    $xmlContent = simplexml_load_file('source.xml');
+?>
+    <?php  
+        echo '<pre>';
+        print_r($xmlContent);
+        echo '</pre>';
+    ?>
+</body>
+</html>
