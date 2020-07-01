b543bb87b32d7b62597947d2b28e65d0022ded99
LewisDchn <lewis.duchesne@outlook.fr>
2020-07-01 12:26:23 +0200

    COMMIT

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <?php 
     $xmlContent = simplexml_load_file('source.xml');
-    $page = 1;
+    include 'controllers/indexController.php';
     $title = $xmlContent->page[$page]->title;
     include 'views/parts/header.php';
-    include 'controllers/indexController.php';
     
 ?>
     <?php  
-<<<<<<< HEAD
-    $page = 0;
-=======
->>>>>>> 1c54000873d1fc2a29ba7d1d903bb48b10741cb0
         echo $xmlContent->page[$page]->menu;
         echo $xmlContent->page[$page]->content;
     ?>
